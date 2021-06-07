ede5572327d3c583acdcf06126a14b86695846a1
Karen Armenakyan <okarenakyano@gmail.com>
2021-06-07 19:02:20 +0300

    example

--- src/org/simply/connected/slae/Main.java
+++ src/org/simply/connected/slae/Main.java
@@ -1,22 +1,23 @@
 package org.simply.connected.slae;
 
-import org.simply.connected.slae.generator.Generator;
-import org.simply.connected.slae.generator.HilbertMatrixLUDecompositionGenerator;
-import org.simply.connected.slae.generator.ProfileMatrixLUDecompositionGenerator;
-import org.simply.connected.slae.methods.LUDecompositionSlaeSolver;
-import org.simply.connected.slae.methods.SlaeSolver;
+import org.simply.connected.slae.math.DenseMatrix;
+import org.simply.connected.slae.math.SquareMatrix;
+import org.simply.connected.slae.math.Vector;
+import org.simply.connected.slae.methods.ConjugateGradientSlaeSolver;
 
-import java.io.IOException;
-import java.nio.file.Path;
+import static org.simply.connected.slae.math.Math.product;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-/*        Generator gen = new HilbertMatrixLUDecompositionGenerator();
-        gen.generate(Path.of("C:\\Users\\okare\\IdeaProjects\\OptMethodsLab3\\Test").toFile(), "HILBERT");*/
-        String path = "C:\\Users\\okare\\IdeaProjects\\OptMethodsLab3\\Test\\HILBERT_10";
-        String solpath = "C:\\Users\\okare\\IdeaProjects\\OptMethodsLab3\\Test\\HILBERT_10_ans";
-        SlaeSolver solver = new LUDecompositionSlaeSolver(1e-3);
-        solver.solveFile(Path.of(path).toFile(), Path.of(solpath).toFile());
+    public static void main(String[] args)  {
+        ConjugateGradientSlaeSolver solver = new ConjugateGradientSlaeSolver();
+        SquareMatrix matrix = new DenseMatrix(
+                new double[]{1, 1./2, 1./3},
+                new double[]{1./2, 1./3, 1./4},
+                new double[]{1./3  , 1./4, 1./5}
+        );
+        Vector f = product(matrix, new Vector(1, 2, 3));
+        System.out.println(solver.solve(matrix, f));
+        System.out.println(solver.getIterations());
     }
 }
